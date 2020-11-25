962ed76b7aad4d8c10e59b538cbfb0c3d971221d
Olo <aleksander.skorek@gmail.com>
2020-11-25 19:07:26 +0100

    finish mobile layout and js

---
+++ 06-checkout-page-master/app.js
@@ -0,0 +1,33 @@
+const baskets = document.querySelectorAll(".basket__item");
+
+baskets.forEach((basket) => basket.addEventListener("click", addRemoveProduct));
+
+function addRemoveProduct(e) {
+  const btn = e.target.closest(".basket__btn");
+  if (!btn) return;
+  const amount = e.currentTarget.querySelector(".basket__amount");
+  let amountVaule = amount.textContent;
+  if (btn.matches(".plus")) {
+    amountVaule++;
+    amount.textContent = amountVaule;
+  }
+
+  if (btn.matches(".minus")) {
+    if (amountVaule < 2) return;
+    amountVaule--;
+    amount.textContent = amountVaule;
+  }
+  countTotalPrice();
+}
+
+function countTotalPrice() {
+  const totalPrice = document.querySelector(".total-price");
+  const prices = document.querySelectorAll(".price");
+  const amounts = document.querySelectorAll(".basket__amount");
+  const sumOfPrices = [...prices].reduce(
+    (total, item, idx) =>
+      total + +item.textContent.slice(1) * +amounts[idx].textContent,
+    0
+  );
+  totalPrice.textContent = `$${(sumOfPrices + 19).toFixed(2)}`;
+}
